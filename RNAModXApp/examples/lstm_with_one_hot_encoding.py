582974b193fd05fb7a120d9a411942c5f10ce3ed
shashi.vish <shashi.vish@yahoo.in>
2023-03-21 17:54:58 +0800

    Initial Commit for PRoject

--- RNAModXApp/examples/lstm_with_one_hot_encoding.py
+++ RNAModXApp/examples/lstm_with_one_hot_encoding.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class LSTM_Model(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
         super(LSTM_Model, self).__init__()
@@ -17,8 +18,10 @@ class LSTM_Model(nn.Module):
         out = self.fc(out[:, -1, :])
         return out
 
+
 # Define a sample RNA sequence
 sequence = 'GGGGCCGTGGATACCTGCCTTTTAATTCTTTTTTATTCGCCCATCGGGGCCGCGGATACCTGCTTTTTATTTTTTTTTCCTTAGCCCATCGGGGTATCGGATACCTGCTGATTCCCTTCCCCTCTGAACCCCCAACACTCTGGCCCATCGGGGTGACGGATATCTGCTTTTTAAAAATTTTCTTTTTTTGGCCCATCGGGGCTTCGGATA'
+
 
 # One-hot encode the RNA sequence
 def one_hot_encode(sequence):
@@ -30,13 +33,15 @@ def one_hot_encode(sequence):
         one_hot.append(hot)
     return one_hot
 
-encoded_sequence = torch.tensor(one_hot_encode(sequence), dtype=torch.float32)
-encoded_sequence = encoded_sequence.unsqueeze(0) # add batch dimension
 
-input_dim = 4 # one-hot encoding of RNA nucleotides
+one_hot_encoded_data = one_hot_encode(sequence)
+encoded_sequence = torch.tensor(one_hot_encoded_data, dtype=torch.float32)
+encoded_sequence = encoded_sequence.unsqueeze(0)  # add batch dimension
+
+input_dim = 4  # one-hot encoding of RNA nucleotides
 hidden_dim = 128
 layer_dim = 2
-output_dim = 2 # binary classification of modification presence or absence
+output_dim = 4  # binary classification of modification presence or absence
 
 model = LSTM_Model(input_dim, hidden_dim, layer_dim, output_dim)
 
@@ -45,7 +50,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 # Replace the following line with your actual training data
 X_train = encoded_sequence
-y_train = torch.tensor([1], dtype=torch.long) # Replace this with your actual target labels
+y_train = torch.tensor([1], dtype=torch.long)  # Replace this with your actual target labels
 
 # Train the model using the training dataset
 for epoch in range(100):
@@ -58,11 +63,11 @@ for epoch in range(100):
     loss.backward()
     optimizer.step()
 
-    if (epoch+1) % 10 == 0:
-        print ('Epoch [{}/100], Loss: {:.4f}'.format(epoch+1, loss.item()))
+    if (epoch + 1) % 10 == 0:
+        print('Epoch [{}/100], Loss: {:.4f}'.format(epoch + 1, loss.item()))
 
 # Replace the following line with your actual target labels
-y_train = torch.tensor([1], dtype=torch.long)
+y_train = torch.tensor([1, 0, 0, 0], dtype=torch.long)
 
 with torch.no_grad():
     outputs = model(X_train)
@@ -70,4 +75,3 @@ with torch.no_grad():
     accuracy = torch.sum(predicted.eq(y_train)).item() / y_train.shape[0]
 
 print(f'Accuracy: {accuracy:.2f}')
-
