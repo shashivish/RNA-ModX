38ee4c3d4a3b90e233c99f52fcfef220474c001d
shashi.vish <shashi.vish@yahoo.in>
2023-03-27 12:25:43 +0800

    Updated Windows Size Configuration for Model Prediction

--- RNAModXApp/webapp/main.py
+++ RNAModXApp/webapp/main.py
@@ -1,12 +1,13 @@
 import pickle
 import json
-from model_files.ml_model import predict_rna_modification_status
+from model_files.ml_model import ModelPredictionHelperClass
 
 from flask import Flask, jsonify, request, render_template, flash
 
 ##creating a flask app and naming it "app"
 app = Flask('app')
 app.config['SECRET_KEY'] = 'mysecretkey'
+
 
 @app.route('/')
 def index():
@@ -21,53 +22,27 @@ def test():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
+
+        MODEL_FILE_NAME = 'xgboost_model.bin'
         rna_sequence = request.form.get('RNA Sequence')
+        window_size = request.form.get('Window Size')
 
         if not rna_sequence:
             flash('RNA Sequence is required!')
         else:
-            print("Begin Prediction")
-            print("---------------------------------------------")
-            print(rna_sequence)
-            print("---------------------------------------------")
-
             # data = json.loads(rna_sequence)
-            with open('./model_files/model.bin', 'rb') as f_in:
+            with open('./model_files/' + MODEL_FILE_NAME, 'rb') as f_in:
                 model = pickle.load(f_in)
                 f_in.close()
 
             print("Model Loaded Successfully.")
-            predictions = predict_rna_modification_status(rna_sequence, model)
+            cls = ModelPredictionHelperClass()
+            predictions = cls.predict_rna_modification_status(rna_sequence, model, window_size)
 
-            for p in predictions:
-                r = int(p)
-            result = {
-                'model_prediction': r
-            }
+
             # return jsonify(result)
-            return render_template('prediction.html', prediction=r)
+            return render_template('prediction.html', prediction=predictions)
     return render_template('index.html')
-
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     rna_sequence = request.get_json()
-#     print("---------------------------------------------")
-#     print(rna_sequence)
-#     print("---------------------------------------------")
-#
-#     # data = json.loads(rna_sequence)
-#     with open('./model_files/model.bin', 'rb') as f_in:
-#         model = pickle.load(f_in)
-#         f_in.close()
-#     predictions = predict_rna_modification_status(rna_sequence['input'], model)
-#
-#     for p in predictions:
-#         r = int(p)
-#     result = {
-#         'model_prediction': r
-#     }
-#     return jsonify(result)
 
 
 if __name__ == '__main__':
