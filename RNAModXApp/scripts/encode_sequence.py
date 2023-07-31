a34588809d583db4580c44c4654c4f5bdc1f1001
shashi.vish <shashi.vish@yahoo.in>
2023-07-31 23:24:02 +0800

    Function to get probabilities

--- RNAModXApp/scripts/encode_sequence.py
+++ RNAModXApp/scripts/encode_sequence.py
@@ -54,7 +54,7 @@ def encode_sequence(rna_sequence: str, encoding_file_path: str):
     except Exception as e:
         raise ValueError("An error occurred while loading the file: " + str(e))
 
-#     print(f"Encoding file successfully loaded.")
+    #     print(f"Encoding file successfully loaded.")
 
     if len(rna_sequence) != 101:
         raise ValueError('Invalid Sequence Length. Expected Sequence Length is 101.')
@@ -64,29 +64,27 @@ def encode_sequence(rna_sequence: str, encoding_file_path: str):
 
     return X_encoded
 
+'''
 
-if __name__ == '__main__':
-    encoding_file = './3-mer-dictionary.pkl'
-    sequence = 'GGGGCCGTGGATACCTGCCTTTTAATTCTTTTTTATTCGCCCATCGGGGCCGCGGATACCTGCTTTTTATTTTTTTTTCCTTAGCCCATCGGGGTATCGGCTGCA'
-    
+'''
+def get_predictions(sequence, encoding_file):
+    response = {}
     i = 0
-    while i+101 <= len(sequence):
-        subseq = sequence[i:i+101]
+    while i + 101 <= len(sequence):
+        subseq = sequence[i:i + 101]
         print('predict for sub sequence:', subseq)
-    
+
         x_train = encode_sequence(subseq, encoding_file)
-#         print(x_train)
+        #         print(x_train)
 
-        prediction_class = ['hAm','hCm','hGm','hTm','hm1A','hm5C','hm5U','hm6A','hm6Am','hm7G','hPsi','Atol']
-
+        prediction_class = ['hAm', 'hCm', 'hGm', 'hTm', 'hm1A', 'hm5C', 'hm5U', 'hm6A', 'hm6Am', 'hm7G', 'hPsi', 'Atol']
+        list_of_probabilities_for_each_class = []
         for c in prediction_class:
+            data = {}
             model_path = "../model/" + c + "_model.pt"
 
             # If you have GPU then remove map location parameter
             model = torch.load(model_path, map_location=torch.device('cpu'))
-    #         print(model)
-
-    #         print("Shape of X_train ", x_train.shape)
 
             # 0  - Non Modified RNA Nucleoside
             # 1  - Corresponding Modified Nucleoside
@@ -94,9 +92,29 @@ if __name__ == '__main__':
             model.eval()
             with torch.no_grad():
                 output = model(x_train)
-        #         print("Raw Output : ", output)
+                #         print("Raw Output : ", output)
                 probabilities = torch.sigmoid(output)
                 print("Probabilities : ", probabilities)
                 predicted_class = (probabilities > 0.5).float()
+
+                if predicted_class == 1:
+                    # Modified Nucleoside Predicted
+                    data[c] = probabilities
+                else:
+                    if probabilities < 0:
+                        data['NonMod'] = 0
+                    else:
+                        data['NonMod'] = abs(1 - probabilities)  # Keeping Probability positive
+
+            list_of_probabilities_for_each_class.append(data)
+
+        response[subseq] = list_of_probabilities_for_each_class
         #         print("Predicted Class : ", predicted_class)
-        i+=1
+        i += 1
+    return response
+
+
+if __name__ == '__main__':
+    encoding_file = './3-mer-dictionary.pkl'
+    sequence = 'GGGGCCGTGGATACCTGCCTTTTAATTCTTTTTTATTCGCCCATCGGGGCCGCGGATACCTGCTTTTTATTTTTTTTTCCTTAGCCCATCGGGGTATCGGCTGCA'
+    response = get_predictions(sequence, encoding_file)
