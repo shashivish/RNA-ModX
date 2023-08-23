f7893c9c8fe8ab2167c3547029fcc4c6e9cbed7f
Mamtha <mamtha3005@gmail.com>
2023-08-23 17:33:49 +0800

    Add streamlit configuration

--- RNAModXApp/webapp/main.py
+++ RNAModXApp/webapp/main.py
@@ -1,65 +1,28 @@
-import pickle
-import json
-from model_files.ml_model import ModelPredictionHelperClass
+import streamlit as st
 
-from flask import Flask, jsonify, request, render_template, flash
+from RNAModXApp.scripts.encode_sequence import RNAPredictor
+from format_results import format_to_json, save_json_to_excel
 
-##creating a flask app and naming it "app"
-app = Flask('app')
-app.config['SECRET_KEY'] = 'mysecretkey'
-app.jinja_env.globals.update(len=len, int=int , zip=zip, str=str , split=str.split)
+st.title("Let's predict RNA Sequences!")
 
+# Adding a text input widget
+rna_sequence = st.text_input("Enter RNA Sequence", "Type here...")
 
-@app.route('/')
-def index():
-    return render_template('RNAInput.html')
+# Make prediction when user submits input
+if st.button("Predict"):
+    # Process the input using the ML model
+    encoding_file = './3-mer-dictionary.pkl'
+    format_results = 'GGGGCCGTGGATACCTGCCTTTTAATTCTTTTTTATTCGCCCATCGGGGCCGCGGATACCTGCTTTTTATTTTTTTTTCCTTAGCCCATCGGGGTATCGGATACCTGCTGATTCCCTTCCCCTCTGAACCCCCAACACTCTGGCCCATCGGGGTGACGGATATCTGCTTTTTAAAAATTTTCTTTTTTTGGCCCATCGGGGCTTCGGATA'
 
+    rna_predictor = RNAPredictor(encoder_file_path=encoding_file, model_directory_path='../model')
+    response = rna_predictor.get_predictions(format_results)
+    print(response)
 
-@app.route('/about', methods=['GET', 'POST'])
-def test():
-    return render_template('index.html')
+    formatted_result = format_to_json(response)
 
+    st.write("Sequence:", response)
+    # Display the formatted result as JSON
+    st.table(formatted_result)
 
-# @app.route('/predict', methods=['GET', 'POST'])
-# def predict():
-#     if request.method == 'POST':
-#
-#         MODEL_FILE_NAME = 'xgboost_model.bin'
-#         rna_sequence = request.form.get('RNA Sequence')
-#         window_size = request.form.get('Window Size')
-#
-#         if not rna_sequence:
-#             flash('RNA Sequence is required!')
-#         else:
-#             # data = json.loads(rna_sequence)
-#             with open('./model_files/' + MODEL_FILE_NAME, 'rb') as f_in:
-#                 model = pickle.load(f_in)
-#                 f_in.close()
-#
-#             print("Model Loaded Successfully.")
-#             cls = ModelPredictionHelperClass()
-#             predictions = cls.predict_rna_modification_status(rna_sequence, model, window_size)
-#
-#             # return jsonify(result)
-#             return render_template('prediction.html', rna_sequence=rna_sequence, position=predictions)
-#     return render_template('index.html')
-
-@app.route('/api/predict', methods=['POST'])
-def predict():
-    sequence = request.json["sequence"]
-    # Run RNA modification prediction algorithm
-    positions = [10, 15, 20]  # Example result
-    mod_types = ["hAm", "m6A", "Ψ"]  # Example result
-    return jsonify({"positions": positions, "mod_types": mod_types, "sequence": sequence})
-
-
-@app.route("/result")
-def result():
-    positions = request.args.getlist("positions")
-    mod_types = request.args.getlist("mod_types")
-    sequence = request.args.get("sequence")
-    return render_template("result.html", positions=positions, mod_types=mod_types, sequence=sequence)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=9696)
+    # Save the JSON data to an Excel file
+    save_json_to_excel(formatted_result, "sentiment_results.xlsx")
