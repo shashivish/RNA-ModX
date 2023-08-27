3c7c77d85e0748ff88b7b8821186c0e05da35620
Mamtha <mamtha3005@gmail.com>
2023-08-27 15:22:30 +0800

    Comment out everything in main.py for now

--- RNAModXApp/main.py
+++ RNAModXApp/main.py
@@ -22,28 +22,28 @@ if os.path.exists(relative_path):
 else:
     st.text("File does not exist")
     st.text(relative_path)
-# st.title("Let's predict RNA Sequences!")
-#
-# # Adding a text input widget
-# rna_sequence = st.text_input("Enter RNA Sequence", "Type here...")
-#
-# # Make prediction when user submits input
-# if st.button("Predict"):
-#     print([rna_sequence][0])
-#     encoding_file = './3-mer-dictionary.pkl'
-#
-#     rna_predictor = RNAPredictor(encoder_file_path=encoding_file, model_directory_path=os.path.join(current_directory, "model"))
-#     response = rna_predictor.get_predictions([rna_sequence][0])
-#
-#     print('response')
-#     print(response)
-#
-#     formatted_result = format_to_json(response)
-#
-#     st.write("Sequence:", [rna_sequence][0])
-#
-#     # Display the formatted result as JSON
-#     st.dataframe(formatted_result)
-#
-#     # Save the JSON data to an Excel file
-#     save_json_to_excel(formatted_result, "rna-sequence_prediction_results.xlsx")
+st.title("Let's predict RNA Sequences!")
+
+# Adding a text input widget
+rna_sequence = st.text_input("Enter RNA Sequence", "Type here...")
+
+# Make prediction when user submits input
+if st.button("Predict"):
+    print([rna_sequence][0])
+    encoding_file = './3-mer-dictionary.pkl'
+
+    rna_predictor = RNAPredictor(encoder_file_path=encoding_file, model_directory_path=os.path.join(os.path.dirname(script_path), "model"))
+    response = rna_predictor.get_predictions([rna_sequence][0])
+
+    print('response')
+    print(response)
+
+    formatted_result = format_to_json(response)
+
+    st.write("Sequence:", [rna_sequence][0])
+
+    # Display the formatted result as JSON
+    st.dataframe(formatted_result)
+
+    # Save the JSON data to an Excel file
+    save_json_to_excel(formatted_result, "rna-sequence_prediction_results.xlsx")
