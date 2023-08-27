2586386cde966b6c5438e210ad64dc94672e5ae4
Mamtha <mamtha3005@gmail.com>
2023-08-27 15:33:22 +0800

    Remove extra code

--- RNAModXApp/main.py
+++ RNAModXApp/main.py
@@ -1,34 +1,10 @@
 import streamlit as st
-
+import os
 from encode_sequence import RNAPredictor,RNAClassifier, RNATransformerModel
 from format_results import format_to_json, save_json_to_excel
 
-import os
-
-current_directory = os.getcwd()
-
 # Get the path of the current script
 script_path = os.path.abspath(__file__)
-
-data_file_path = os.path.join(os.path.dirname(script_path), "3-mer-dictionary.pkl")
-st.text(data_file_path)
-if os.path.exists(data_file_path):
-    st.text("File exists")
-    st.text(data_file_path)
-else:
-    st.text("File does not exist")
-    st.text(data_file_path)
-
-# Construct relative path to a file (assuming it's in the same directory as the script)
-relative_path = os.path.join(os.path.dirname(script_path), "model", "XGB_OverallBinary.pkl")
-st.text(relative_path)
-if os.path.exists(relative_path):
-    st.text("File exists")
-    st.text(relative_path)
-else:
-    st.text("File does not exist")
-    st.text(relative_path)
-st.title("Let's predict RNA Sequences!")
 
 # Adding a text input widget
 rna_sequence = st.text_input("Enter RNA Sequence", "Type here...")
