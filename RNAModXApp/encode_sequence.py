18ed09b7ad637ccce727bef4601b76946b7901c6
Mamtha <mamtha3005@gmail.com>
2023-09-06 23:38:28 +0800

    Try again

--- RNAModXApp/encode_sequence.py
+++ RNAModXApp/encode_sequence.py
@@ -157,7 +157,7 @@ class RNAPredictor():
         with open(model_path, 'rb') as file:
             xgb_model = pickle.load(file)
 
-        overall_encoder_file_path = "3-mer-dictionary.pkl"
+        overall_encoder_file_path = "./3-mer-dictionary.pkl"
         kmer_dict = self.get_overall_binary_encode(overall_encoder_file_path)
         x_encoded = self.encode_with_k_mer_codon(rna_sequence, kmer_dict, k)
 
